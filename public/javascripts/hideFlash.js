948635a90182b27ee582b242257bcdecd585e574
Yara Horany <yara.horany1@gmail.com>
2022-03-29 10:51:28 +0300

    setTimeout for Flash

---
+++ public/javascripts/hideFlash.js
@@ -0,0 +1,2 @@
+window.setTimeout("document.getElementById('successMessage').style.display='none';", 5000);
+window.setTimeout("document.getElementById('errorMessage').style.display='none';", 5000); 
